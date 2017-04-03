30c21e9892d41595a7a749c27ec8fbb8268f0898
Tayseer <taedouni@gmail.com>
2017-04-02 23:16:04 -0700

    json server is working

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -2,7 +2,7 @@
     'use strict';
     var FORM_SELECTOR = '[data-coffee-order="form"]';
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-    var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
+    var SERVER_URL = 'http://localhost:3002/coffeeorders';
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
